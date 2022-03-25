4b1c8351d91ee0e8cc4097412edc10e9d79586d1
JonathanRoth11 <rj5532@mci4me.at>
2022-03-25 01:09:13 +0100

    power_plotly.py

---
+++ PÜ2/power_plotly.py
@@ -0,0 +1,20 @@
+# %% Import der nötigen Pakete
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% Öffnen der Datei und konvertieren zu numpy-Array
+#%% Durchlaufen der Zelle mithilfe eines for-loops, um die 3 Graphen nacheinander zu plotten
+
+def performance_data():
+    for i in range (1,4):
+        file_name =  ('input_data/power_data_{0}.txt'.format(i))
+        power_data_watts = open(file_name).read().split("\n")
+        x = np.array(power_data_watts)
+        plt.title(("Line graph {0}").format(i))
+        plt.plot(x, color="red")
+
+        plt.show()
+
+# %% Erstellen des Plots
+performance_data()
+# %%
