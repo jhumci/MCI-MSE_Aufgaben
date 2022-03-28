4d27a057a5431872c91c6effadac345e91724ac2
SimonFreilinger <s.freili@gmx.at>
2022-03-28 21:46:28 +0200

    Update 1

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -2,22 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-## begin for-loop (2 bis 3 zeilen hinzufügen)
-## mit for loop text dateine einlesen
-
 # %% Öffnen der Datei und konvertieren zu numpy-Array
-file_name =  'input_data/power_data_1.txt'
-power_data_watts = open(file_name).read().split("\n")
-x = np.array(power_data_watts)
 
-# %% Erstellen des Plots
-plt.title("Line graph")
-plt.plot(x, color="red")
+for i in range (1,4):
+    file_name =  'input_data/power_data_' +str(i) + '.txt'
+    power_data_watts = open(file_name).read().split("\n")
+    x = np.array(power_data_watts)
 
-plt.show()
+    plt.title("Line graph")
+    plt.plot(x, color="red")
 
-## ende for-loop
-# %%
-
-# %%
+    plt.show()
