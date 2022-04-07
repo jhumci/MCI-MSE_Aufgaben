45df41713508b0f367ac35d8969bd1d2146b012d
detzxav <xaver.detzlhofer@gmx.at>
2022-04-07 15:39:17 +0200

    commit

--- PÜ5/main_4_object_oriented.py
+++ PÜ5/main_4_object_oriented.py
@@ -224,10 +224,17 @@ iterator = 0                                        # Zähler, der die gefundene
 for test in list_of_new_tests:                      # Alle Tests werden nacheinander durchlaufen
     test.create_hr_data()                           # Erstelle Herzraten aus den EKG-Daten
     test.add_subject(list_of_subjects[iterator])    # Fügt einem Test die passenden Versuchspersonen hinzu
-
+    test.add_power_data(list_of_power_data[iterator])
+    test.evaluate_termination()
+    test.create_summary()
+    test.ask_for_termination()
+    test.create_plot()
+    test.save_data()
     """
     Fügen Sie hier den Programmablauf ein, indem Sie die Methoden und Klassen von oben nutzen
     """
 
     iterator = iterator + 1
 
+
+# %%
